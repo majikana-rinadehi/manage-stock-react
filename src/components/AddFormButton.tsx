6df197a9d634d5e8533bfb64db4c3e9ebdcbf000
NH <rudyrudy2103@gmail.com>
2022-08-11 13:48:35 +0900

    カテゴリCRUDついか、ADDFORMBUTTONしゅうせい

--- src/components/AddFormButton.tsx
+++ src/components/AddFormButton.tsx
@@ -13,54 +13,38 @@ export const AddFormButton: FunctionComponent<Props> = ({ itemName }) => {
             {
                 !edit
                     ? <div
-                        // v-show="!show"
                         className="flex mx-2 h-12 rounded-lg">
                         <button
-                            // ref="buttonRef"
-                            // @click="showInput"
                             onClick={() => setEdit(true)}
                             className={'w-full hover:bg-gray-300'}
                         >
-                            {/* <!--アイテムを追加--> */}
                             {'＋' + itemName + 'を追加'}
                         </button>
                     </div >
                     :
                     <div
-                        // v-show="show"
                         className="flex justify-end mx-2 hover:bg-gray-300 rounded-lg">
                         <div className="justify-self-start mr-auto p-2">
                             {/* <!-- 何故か、@keydown.enter にすると、押下後「+アイテムを追加」にフォーカス行かない --> */}
                             <input
-                                //   ref="inputRef" 
-                                //   @keydown.enter="addItem"
-                                //   @compositionstart="compositionStart"
-                                //   @compositionend="compositionEnd"
-                                //   @input="inputEvent"
                                 className="w-full h-6 p-2"
                                 type="text"
-                                placeholder="新しいアイテム名"
-                            //   v-model="item_name"
+                                placeholder={'新しい' + itemName + 'を追加'}
                             />
-                            {/* <!--新しいアイテムを追加--> */}
                         </div>
                         <div className="flex m-2">
                             <button
-                                // @click="addItem"
                                 onClick={() => setEdit(false)}
                                 className="px-3 py-2 bg-green-500 hover:bg-green-700 
-            text-white rounded-lg mr-2 font-bold text-xs">
+                                        text-white rounded-lg mr-2 font-bold text-xs">
                                 追加
                             </button>
-                            {/* <!--追加--> */}
                             <button
-                                // @click="closeInput"
                                 onClick={() => setEdit(false)}
                                 className="px-3 py-2 bg-red-500 hover:bg-red-700 
-          text-white rounded-lg font-bold text-xs">
+                                        text-white rounded-lg font-bold text-xs">
                                 キャンセル
                             </button>
-                            {/* <!--キャンセル--> */}
                         </div >
                     </div >
             }
--- src/features/categories/categoriesSlice.ts
+++ src/features/categories/categoriesSlice.ts
@@ -1,6 +1,7 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import type { Category } from "@/common/types";
 import { RootState } from '@/app/store'
+import { format } from 'date-fns'
 
 const initCategories: Category[] = [
     {
@@ -28,10 +29,39 @@ export const categoriesSlice = createSlice({
     name: 'categories',
     initialState: initCategories,
     reducers: {
+        addCategory: (state, action: PayloadAction<{newCategoryName: string}>) => {
+            state.push({
+                categoryId: format(new Date(), 'yyyyMMddhhmmss'),
+                categoryName: action.payload.newCategoryName,
+                addDate: format(new Date(), 'yyyyMMdd'),
+                updDate: format(new Date(), 'yyyyMMdd'),
+                items: []
+            })
+        },
+        deleteCategory: (state, action: PayloadAction<{categoryId: string}>) => {
+            state.splice(
+                state.findIndex(category => 
+                    category.categoryId === action.payload.categoryId)
+            )
+        },
+        updateCategory: (state, action: PayloadAction<{newCategoryName: string, categoryId: string}>) => {
+            state = state.map(category => {
+                if (category.categoryId === action.payload.categoryId) {
+                    category.categoryName = action.payload.newCategoryName
+                }
+                return category
+            })
+        },
 
     }
 })
 
 export default categoriesSlice.reducer
+
+export const {
+    addCategory,
+    deleteCategory,
+    updateCategory
+} = categoriesSlice.actions
 
 export const selectCategories = (state: RootState) => state.categories
